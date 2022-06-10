8a3aa3605285fef66fb6d6f747bbc25f661785a8
Priscila Flores <priscilauchoaa@gmail.com>
2022-06-10 23:15:23 +0200

    create repository for product

---
+++ backend/src/repository/product-repository.js
@@ -0,0 +1,46 @@
+const moment = require("moment");
+const { v4 } = require("uuid");
+
+let products = [
+    {
+        // id: v4(),
+        id: "UNIQUE",
+        name: "Storage Jar",
+        quantity: 6,
+        barcode: "32874627567862786",
+        active: true,
+        date: moment(),
+    },
+];
+
+const update = (id, product) => {
+    let productToUpdate = getById(id);
+
+    products[products.indexOf(productToUpdate)] = {
+        ...productToUpdate,
+        ...product,
+        id,
+    };
+};
+
+const getAll = () => products.filter((product) => product.active);
+
+const create = (product) => {
+    const entity = {
+        ...product,
+        id: v4(),
+        date: moment(),
+        active: true
+    };
+    products.unshift(entity);
+    return entity;
+};
+
+const getById = (id) => products.find((product) => product.id === id);
+
+module.exports = {
+    create,
+    getById,
+    update,
+    getAll,
+};
