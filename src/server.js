7a409f6928a4ad9279b57a7e05e70598a10f9ff6
Priscila Flores <priscilauchoaa@gmail.com>
2022-06-11 15:56:06 +0200

    add morgan to see the request log

--- src/server.js
+++ src/server.js
@@ -1,14 +1,12 @@
 const express = require("express");
-const app = express();
-
+const morgan = require("morgan");
 const cors = require("cors");
-
 const bodyParser = require("body-parser");
-
 const api = require("./api/index");
 
+const app = express();
+app.use(morgan("tiny"));
 app.use(cors());
-
 app.use(bodyParser.json());
 
 app.use("/api", api);
